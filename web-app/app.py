1ff9f3100f9944a14a1ee13b862d42ee2ad5eea0
Junyi Dong <60515732+junyid@users.noreply.github.com>
2022-11-28 14:18:06 -0500

    Web UI base

---
+++ web-app/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from dotenv import dotenv_values
+
+import pymongo
+import datetime
+from bson.objectid import ObjectId
+import sys
+
+# instantiate the app
+app = Flask(__name__)
+
+# load credentials and configuration options from .env file
+# if you do not yet have a file named .env, make one based on the template in env.example
+config = dotenv_values(".env")
+
+# turn on debugging if in development mode
+if config['FLASK_ENV'] == 'development':
+    # turn on debugging, if in development
+    app.debug = True # debug mnode
+
+
+# connect to the database
+cxn = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
+try:
+    # verify the connection works by pinging the database
+    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
+    db = cxn[config['MONGO_DBNAME']] # store a reference to the database
+    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+except Exception as e:
+    # the ping command failed, so the connection is not available.
+    print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
+    print('Database connection error:', e) # debug
+
+# set up the routes
+
+# route for the home page
+@app.route('/')
+def home():
+    """
+    Route for the home page
+    """
+    return render_template('index.html') # render the hone template
+
+# route to handle any errors
+@app.errorhandler(Exception)
+def handle_error(e):
+    """
+    Output any errors - good for debugging.
+    """
+    return render_template('error.html', error=e) # render the edit template
+
+
+# run the app
+if __name__ == "__main__":
+    app.run(debug = True)
