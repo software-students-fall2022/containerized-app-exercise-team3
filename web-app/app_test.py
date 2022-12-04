c351c3207c9cf791c951d0dc02729755c16c0e2e
Junyi Dong <60515732+junyid@users.noreply.github.com>
2022-12-04 15:20:13 -0500

    tests for the web app

--- web-app/app_test.py
+++ web-app/app_test.py
@@ -1,45 +1,26 @@
-from app import configure_routes
-from flask import Flask, render_template
-import pytest
-import pytest_flask
-import pymongo
-import mongomock
+import requests
 
-collection = mongomock.MongoClient().db.collection
+port = 5000
+base = "http://localhost:" + str(port) + "/"
 
 def test_base_template():
-    app = configure_routes(Flask(__name__))
-    client = app.test_client()
-    url = '/'
-    response = client.get(url)
+    url = base
+    response = requests.get(url)
     assert response.status_code == 200
 
-def test_index_template():
-    app = configure_routes(Flask(__name__))
-    client = app.test_client()
-    url = '/index'
-    response = client.get(url)
-    assert response.status_code == 404
+def test_index_template(): # ??? same route as base
+    url = base
+    response = requests.get(url)
+    assert response.status_code == 200
 
 def test_job_template():
-    app = configure_routes(Flask(__name__))
-    client = app.test_client()
-    url = '/job'
-    response = client.get(url)
-    assert response.status_code == 404
+    url = base + 'job/'
+    response = requests.get(url)
+    assert response.status_code == 200
 
 def test_error_template():
-    app = configure_routes(Flask(__name__))
-    client = app.test_client()
-    url = '/error'
-    response = client.get(url)
-    assert response.status_code == 404
-
-def test_base_template():
-    app = configure_routes(Flask(__name__))
-    client = app.test_client()
-    url = '/base'
-    response = client.get(url)
-    assert response.status_code == 404
+    url = base + 'error'
+    response = requests.get(url)
+    assert response.status_code == 200
 
 
